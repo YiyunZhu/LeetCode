a68a308f7ebf0b063bdbc7bc085acbc8750913b4
YiyunZhu <zhuy3_15@uni.worc.ac.uk>
2018-07-10 19:53:58 +0100

    update solution387.py

--- 387.First_Unique_Character_in_a_String/solution387.py
+++ 387.First_Unique_Character_in_a_String/solution387.py
@@ -1,10 +1,10 @@
 class Solution:
     def firstUniqChar(self, s):
-        #     # #It works but for long string, time limited exceeded
-    #     # for i in range(len(str)):
-    #     #     while str.count(str[i])==1:
-    #     #         return i
-    #     # return -1
+        # #It works but for long string, time limited exceeded
+        # for i in range(len(str)):
+        #     while str.count(str[i])==1:
+        #         return i
+        # return -1
         ch=[]
         for i in set(s):
             if s.count(i) == 1:
@@ -13,14 +13,3 @@ class Solution:
             return min(ch)
         else:
             return -1
-
-    
-    
-def main():
-    # input="dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"
-    s="absd"
-    print(Solution().firstUniqChar(s))
-    
-    
-if __name__ == '__main__':
-    main()
